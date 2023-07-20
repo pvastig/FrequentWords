cf45426ea956b14cbb4cb772bf5e0d2f73a97d7a
Pavel <pvastig@gmail.com>
2023-07-21 00:51:52 +0700

    Added trasformig to lowcase, added test

--- lib/FrequentWords.cpp
+++ lib/FrequentWords.cpp
@@ -2,13 +2,19 @@
 
 #include <regex>
 
+std::string strTolower(std::string s)
+{
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
+
+    return s;
+}
+
 std::map<std::string, int> pa::getMappedWords(const std::string& words)
 {
     std::map<std::string, int> wordsToCount;
     const std::regex regex("\\w+");
     for (std::sregex_iterator it(words.begin(), words.end(), regex), end; it != end; ++it) {
-        std::smatch match(*it);
-        const auto insertedIt = wordsToCount.emplace(match.str(), 1);
+        const auto insertedIt = wordsToCount.emplace(strTolower(std::smatch(*it).str()), 1);
         if (!insertedIt.second) {
             ++insertedIt.first->second;
         }
--- tests/FrequentWordsTests.cpp
+++ tests/FrequentWordsTests.cpp
@@ -7,6 +7,18 @@ namespace pa {
 TEST(FrequentWords, SimpleCases)
 {
     {
+        const std::string text;
+        const auto words = getMappedWords(text);
+        ASSERT_EQ(words.size(), 0);
+    }
+    {
+        const std::string text = "a";
+        const auto words = getMappedWords(text);
+        ASSERT_EQ(words.size(), 1);
+        ASSERT_EQ(words.cbegin()->first, "a");
+        ASSERT_EQ(words.cbegin()->second, 1);
+    }
+    {
         const std::string text = "a a a";
         const auto words = getMappedWords(text);
         ASSERT_EQ(words.size(), 1);
@@ -28,6 +40,7 @@ TEST(FrequentWords, SimpleCases)
         ASSERT_EQ(words.cbegin()->second, 3);
     }
 }
+
 TEST(FrequentWords, CamelCases)
 {
     {
@@ -36,6 +49,41 @@ TEST(FrequentWords, CamelCases)
         ASSERT_EQ(words.size(), 1);
         ASSERT_EQ(words.cbegin()->first, "a");
         ASSERT_EQ(words.cbegin()->second, 3);
+    }
+    {
+        const std::string text = "A A A";
+        const auto words = getMappedWords(text);
+        ASSERT_EQ(words.size(), 1);
+        ASSERT_EQ(words.cbegin()->first, "a");
+        ASSERT_EQ(words.cbegin()->second, 3);
+    }
+}
+TEST(FrequentWords, ComplexCases)
+{
+    {
+        std::map<std::string, int> refWords{
+            {"come", 1},
+            {"has", 1},
+            {"many", 1},
+            {"of", 1},
+            {"sad", 1},
+            {"talk", 1},
+            {"the", 2},
+            {"things", 1},
+            {"time", 1},
+            {"to", 1},
+            {"warlis", 1},
+
+        };
+        const std::string text = "The time has come, the Warlis sad to talk of many things...";
+        const auto words = getMappedWords(text);
+        ASSERT_EQ(words.size(), 11);
+        auto refIt = refWords.cbegin();
+        auto it = words.cbegin();
+        for (; refIt != refWords.cend(); ++refIt, ++it) {
+            ASSERT_EQ(refIt->first, it->first);
+            ASSERT_EQ(refIt->second, it->second);
+        }
     }
 }
 
